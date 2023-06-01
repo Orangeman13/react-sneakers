b3882e72dbdb19f9e859dc461ce99fa3e30b1810
Orangeman13 <xyntaa13@icloud.com>
2023-06-02 00:53:40 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
-function App() {
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/pictures/sneakers/s1.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    imageUrl: "/pictures/sneakers/s2.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "/pictures/sneakers/s3.jpg",
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "/pictures/sneakers/s4.jpg",
+  },
+];
+
+function App(props) {
   return (
     <div className="wrapper clear">
       <Drawer />
@@ -15,9 +38,10 @@ function App() {
             <input placeholder="Поиск...." />
           </div>
         </div>
-
         <div className="d-flex">
-          <Card />
+          {arr.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="favorite">
         <img src="/pictures/sneakers/unliked.svg" alt="unlike" />
       </div>
-      <img width={133} height={112} src="/pictures/sneakers/s1.jpg" alt="s1" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="s1" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column ">
           <p>Цена:</p>
-          <b>12 999 rub.</b>
+          <b>{props.price} rub.</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="/pictures/plus.svg" alt="+" />
